5e3d3213873b5043aa0bdd66cc5fc18a10299928
Hasan <hasanhafizov258@gmail.com>
2026-06-03 11:11:37 +0500

    Update checkbox component

--- resources/js/components/ui/checkbox/Checkbox.vue
+++ resources/js/components/ui/checkbox/Checkbox.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
-import type { CheckboxRootProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { Check } from "lucide-vue-next"
-import { CheckboxIndicator, CheckboxRoot } from "reka-ui"
+import { Check } from 'lucide-vue-next';
+import { CheckboxIndicator, CheckboxRoot } from 'reka-ui';
+import type { CheckboxRootProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
 
-const props = defineProps<CheckboxRootProps & { class?: HTMLAttributes["class"] , label: string }>()
+defineProps<
+    CheckboxRootProps & { class?: HTMLAttributes['class']; label?: string }
+>();
 
-const propsModelValue = defineModel<boolean>()
-
+const propsModelValue = defineModel<boolean>();
 </script>
 
 <template>
-<div class="flex flex-col gap-2.5">
-    <label class="flex flex-row gap-4 items-center [&>.checkbox]:hover:bg-neutral-100">
-      <CheckboxRoot
-        v-model="propsModelValue"
-        class="hover:bg-stone-50 flex h-5 w-5 appearance-none items-center justify-center rounded-md bg-white shadow-sm border outline-none"
-      >
-        <CheckboxIndicator class="bg-white h-full w-full rounded flex items-center justify-center">
-          <Check class="size-3.5" />
-        </CheckboxIndicator>
-      </CheckboxRoot>
-      <span class="select-none text-stone-700 text-sm dark:text-white cursor-pointer">{{ label }}</span>
-    </label>
-  </div>
+    <div class="flex flex-col gap-2.5">
+        <label
+            class="flex flex-row items-center gap-4 [&>.checkbox]:hover:bg-neutral-100"
+        >
+            <CheckboxRoot
+                v-model="propsModelValue"
+                class="flex h-5 w-5 appearance-none items-center justify-center rounded-md border bg-white shadow-sm outline-none hover:bg-stone-50"
+            >
+                <CheckboxIndicator
+                    class="flex h-full w-full items-center justify-center rounded bg-white"
+                >
+                    <Check class="size-3.5" />
+                </CheckboxIndicator>
+            </CheckboxRoot>
+            <span
+                v-if="label"
+                class="cursor-pointer text-sm text-stone-700 select-none dark:text-white"
+            >
+                {{ label }}
+            </span>
+        </label>
+    </div>
 </template>
