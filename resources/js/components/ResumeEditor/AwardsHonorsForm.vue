9b3f7acbc2fd72462755c28053994a79b48de9d1
Diana <radchenkodianastudy@gmail.com>
2026-08-23 09:34:06 +0300

    Add resume publications, awards and languages (resources/js/components/ResumeEditor/AwardsHonorsForm.vue)

---
+++ resources/js/components/ResumeEditor/AwardsHonorsForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ResumeDetailListForm from './ResumeDetailListForm.vue';
+defineProps<{ resume: { id: number; title: string }; awardHonors: any[] }>();
+defineEmits<{ nextSection: [section: string] }>();
+</script>
+<template>
+    <ResumeDetailListForm
+        :resume="resume"
+        :items="awardHonors"
+        type="award-honor"
+        previous-section="publications"
+        next-section="languages"
+        @next-section="$emit('nextSection', $event)"
+    />
+</template>
+
