de3a71ce8b48a34417bb2bee3c45110e4098e867
Diana <radchenkodianastudy@gmail.com>
2026-08-23 09:33:20 +0300

    Add resume publications, awards and languages (app/Http/Requests/StorePublicationRequest.php)

---
+++ app/Http/Requests/StorePublicationRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePublicationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'publisher' => 'nullable|string|max:255',
+            'publication_date' => 'nullable|date',
+            'url' => 'nullable|url|max:2048',
+            'description' => 'nullable|string|max:5000',
+        ];
+    }
+}
+
