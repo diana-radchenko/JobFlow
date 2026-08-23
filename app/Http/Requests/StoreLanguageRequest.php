f5d3a6bbfca54a51bcee7945b59194b20e83cdf1
Diana <radchenkodianastudy@gmail.com>
2026-08-23 09:33:13 +0300

    Add resume publications, awards and languages (app/Http/Requests/StoreLanguageRequest.php)

---
+++ app/Http/Requests/StoreLanguageRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreLanguageRequest extends FormRequest
+{
+    public const PROFICIENCIES = ['Native', 'Fluent', 'Professional', 'Intermediate', 'Basic'];
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'proficiency' => ['required', Rule::in(self::PROFICIENCIES)],
+        ];
+    }
+}
+
